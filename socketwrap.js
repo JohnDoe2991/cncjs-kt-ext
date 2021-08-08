c3b2c5214c3ac728df9cc5cd502ca7b5737846f1
Johnny <24862620+JohnDoe2991@users.noreply.github.com>
2021-08-08 21:31:45 +0200

    add debug output for loaded gcode

--- socketwrap.js
+++ socketwrap.js
@@ -5,12 +5,14 @@ module.exports = class SocketWrap {
   }
 
   sendGcode (gcode) {
-    // console.log('sending gcode:', gcode);
+    console.log('sending gcode:', gcode)
     this.socket.emit('command', this.port, 'gcode', gcode)
   }
 
   loadGcode (name, gcode) {
+    console.log("load gcode:", gcode)
     this.socket.emit('command', this.port, 'gcode:load', name, gcode)
+    console.log("gcode loaded")
   }
 
   stopGcode (file, gcode) {
